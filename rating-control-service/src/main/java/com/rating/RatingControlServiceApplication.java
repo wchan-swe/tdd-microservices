38b9649ab6aa7d23063935b6b68ff40b5a50f3a1
Wayne Chan <wchan.it@outlook.com>
2021-08-27 19:45:25 -0500

    initial commit

---
+++ rating-control-service/src/main/java/com/rating/RatingControlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingControlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingControlServiceApplication.class, args);
+    }
+
+}
